0335078a6c4bf49dadcf7d0377919f8cd122318d
Hithesh Jain <138424080+HitheshJain2002@users.noreply.github.com>
2025-01-07 12:50:58 +0530

    fine

--- Ukshati-main/Routes/Authroutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {LoginController} from "../Controllers/Authcontroller.js";
-
-
-const router = express.Router();
-
-router.post('/',LoginController);
-
-export default router
